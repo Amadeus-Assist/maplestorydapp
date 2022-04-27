d69631f79b24bfe4b60b9ce9158b118f4fca966c
CharlesK <xk2144@columbia.edu>
2022-04-27 02:41:14 -0400

    add deposit function to smart contract

--- smart_contract/contracts/MaplesotryNFT.sol
+++ smart_contract/contracts/MaplesotryNFT.sol
@@ -6,23 +6,25 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 // keeps track of the number of tokens issued
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 // Accessing the Ownable method ensures that only the creator of the smart contract can interact with it
-contract MaplestoryDappNFT is ERC721, Ownable {
+contract MaplestoryDappNFT is ERC721 {
     //    using Counters for Counters.Counter;
     //    Counters.Counter private _tokenIds;
-    Weapon[] weapons;
-    address public operatorAddress;
+    Equipment[] equipments;
+    address payable public operatorAddress;
+    mapping(address => uint256) deposit;
     //    bool public paused = false;
     //    mapping(address => uint256) ownershipWeaponCount;
     //    mapping(uint256 => address) weaponIndexToOwner;
 
-    struct Weapon {
-        string weapon_type;
+    struct Equipment {
         string name;
-        uint64 drop_time;
         uint32 attack;
+        uint32 defense;
+        uint32 magic_defense;
+        uint32 power_hit;
+        uint32 drop_time;
     }
 
     // @dev Access modifier for operator-only functionality
@@ -31,73 +33,77 @@ contract MaplestoryDappNFT is ERC721, Ownable {
         _;
     }
 
-    //    modifier whenPaused(){
-    //        require(paused);
-    //        _;
-    //    }
-    //
-    //    modifier whenNotPaused(){
-    //        require(!paused);
-    //        _;
-    //    }
-
     // the name and symbol for the NFT
-    constructor() public ERC721("MaplestoryDappNFT", "MSD") {
-        operatorAddress = msg.sender;
+    constructor() ERC721("MaplestoryDappNFT", "MSD") {
+        operatorAddress = payable(msg.sender);
+        Equipment memory _initEmptyEquipment = Equipment({
+        name : "init",
+        attack : 0,
+        defense : 0,
+        magic_defense : 0,
+        power_hit : 0,
+        drop_time : 0
+        });
+        equipments.push(_initEmptyEquipment);
     }
 
     // Create a function to mint/create the NFT
     // receiver takes a type of address. This is the wallet address of the user that should receive the NFT minted using the smart contract
     // tokenURI takes a string that contains metadata about the NFT
-
-    function createWeaponNFT(address receiver, string memory weapon_type, string memory name, uint256 drop_time,
-        uint256 attack)
+    function createEquipmentNFT(address receiver, string memory name, uint256 attack, uint256 defense, uint256
+        magic_defense, uint256 power_hit, uint256 drop_time, uint256 cost)
     public onlyOperator
     returns (uint256)
     {
         //        _tokenIds.increment();
+        require(deposit[receiver] >= cost, "Insufficient deposit");
+        require(drop_time == uint256(uint32(drop_time)));
+        require(attack == uint256(uint32(attack)));
+        require(defense == uint256(uint32(defense)));
+        require(magic_defense == uint256(uint32(magic_defense)));
+        require(power_hit == uint256(uint32(power_hit)));
 
-        require(drop_time == uint256(uint64(drop_time)));
-        require(drop_time == uint256(uint32(attack)));
-
-        Weapon memory _weapon = Weapon({
-        weapon_type : weapon_type,
+        Equipment memory _equipment = Equipment({
         name : name,
-        drop_time : uint64(drop_time),
-        attack : uint32(attack)
+        attack : uint32(attack),
+        defense : uint32(defense),
+        magic_defense : uint32(magic_defense),
+        power_hit : uint32(power_hit),
+        drop_time : uint32(drop_time)
         });
 
-        weapons.push(_weapon);
-        uint256 newWeaponId = weapons.length - 1;
-        require(newWeaponId == uint256(uint32(newWeaponId)));
+        equipments.push(_equipment);
+        uint256 newEquipmentId = equipments.length - 1;
+        require(newEquipmentId == uint256(uint32(newEquipmentId)));
 
         //        uint256 newItemId = _tokenIds.current();
-        _mint(receiver, newWeaponId);
+        deposit[receiver] -= cost;
+        _safeMint(receiver, newEquipmentId);
         //        _setTokenURI(newItemId, tokenURI);
 
         // returns the id for the newly created token
-        return newWeaponId;
+        return newEquipmentId;
     }
 
     function totalSupply() public view returns (uint) {
-        return weapons.length - 1;
+        return equipments.length - 1;
     }
 
-    function weaponsOfOwner(address _owner) external view returns (uint256[] memory ownerTokens) {
-        uint256 weaponsCount = balanceOf(_owner);
+    function equipmentOfOwner(address _owner) external view returns (uint256[] memory ownerTokens) {
+        uint256 equipmentCount = balanceOf(_owner);
 
-        if (weaponsCount == 0) {
+        if (equipmentCount == 0) {
             return new uint256[](0);
         } else {
-            uint256[] memory result = new uint256[](weaponsCount);
-            uint256 totalWeapons = totalSupply();
+            uint256[] memory result = new uint256[](equipmentCount);
+            uint256 totalEquipment = totalSupply();
             uint256 resultIndex = 0;
 
-            uint256 weaponId;
+            uint256 equipmentId;
 
-            for (weaponId = 1; weaponId <= totalWeapons; weaponId++) {
-                if (ownerOf(weaponId) == _owner) {
-                    result[resultIndex] = weaponId;
+            for (equipmentId = 1; equipmentId <= totalEquipment; equipmentId++) {
+                if (ownerOf(equipmentId) == _owner) {
+                    result[resultIndex] = equipmentId;
                     resultIndex++;
                 }
             }
@@ -105,16 +111,34 @@ contract MaplestoryDappNFT is ERC721, Ownable {
         }
     }
 
-    function getWeapon(uint256 _id) external view returns (
-        string memory weapon_type,
+    function getEquipment(uint256 _id) external view returns (
         string memory name,
-        uint64 drop_time,
-        uint32 attack
+        uint32 attack,
+        uint32 defense,
+        uint32 magic_defense,
+        uint32 power_hit,
+        uint32 drop_time
     ){
-        Weapon storage weapon = weapons[_id];
-        weapon_type = weapon.weapon_type;
-        name = weapon.name;
-        drop_time = weapon.drop_time;
-        attack = weapon.attack;
+        Equipment storage equipment = equipments[_id];
+        name = equipment.name;
+        attack = equipment.attack;
+        defense = equipment.defense;
+        magic_defense = equipment.magic_defense;
+        power_hit = equipment.power_hit;
+        drop_time = equipment.drop_time;
     }
+
+    event successDeposit(string transactionId);
+
+    function depositMoney(address _receiver, string memory transactionId) payable public {
+        deposit[_receiver] += msg.value;
+        require(operatorAddress.send(msg.value));
+        emit successDeposit(transactionId);
+    }
+
+    function getDeposit(address _owner) external view returns(uint256){
+        return deposit[_owner];
+    }
+
+
 }
