3e529dad9534dc76729bedc55068786360f23d10
Guoxin Yin <guoxiny@umich.edu>
2022-05-09 17:23:23 -0400

    hardcode frontend without interacting with smart contract

--- frontend/market/src/components/HelloWorld.vue
+++ frontend/market/src/components/HelloWorld.vue
@@ -4,6 +4,37 @@
     <button v-show="!isConnected" @click="connect">Connect Ale</button>
     <div style="color: #f00" v-show="isConnected">Connect Success!</div>
     <button v-show="isConnected" @click="approveMoney">approve $100</button>
+
+  <div class='list-group'>
+    This is the list of all onsell equipments.
+  <a v-for='(product, idx) in allonsell' :key="idx" class='list-group-item'
+     v-on:click='toggleActiveIndex(allonsell, idx)' 
+     :class="{'active': idx == activeIndex && allonsell == activeList}" >
+  {{product}}
+  </a>
+  </div>
+  <button  @click="buyequipment">Buy</button>
+
+  <div class='list-group'>
+    This is the list of all of YOUR onsell equipments.
+     <a v-for='(product, idx) in youronsell' :key="idx" class='list-group-item'
+        v-on:click='toggleActiveIndex(youronsell, idx)' 
+        :class="{'active': idx == activeIndex && youronsell == activeList}" >
+     {{product}} 
+     </a>
+  </div>
+  <button  @click="cancelsell">Cancel the Sell</button>
+
+  <div class='list-group'>
+    This is the list of all of YOUR other equipments.
+     <a v-for='(product, idx) in yourother' :key="idx" class='list-group-item'
+        v-on:click='toggleActiveIndex(yourother, idx)' 
+        :class="{'active': idx == activeIndex && yourother == activeList}" >
+     {{product}} 
+     </a>
+  </div>
+  <input v-model="sellprice" placeholder="Your Price">
+  <button  @click="sellequipment">Sell</button>
   </div>
 </template>
 
@@ -16,6 +47,14 @@ export default {
     return {
       account: "",
       isConnected: false,
+
+      activeIndex: null,
+      activeList: null,
+      allonsell: ['a', 'b', 'c'],
+      youronsell: ['1244', '125215', '02150'],
+      yourother: ['1244', '125215', '02150'],
+
+      sellprice: ""
     };
   },
   watch: {
@@ -40,6 +79,25 @@ export default {
         console.log(res);
       });
     },
+    toggleActiveIndex: function(list,index){
+     this.activeIndex = index;
+     this.activeList = list;
+   },
+    buyequipment() {
+      console.log("buy the onsell equipment")
+    },
+    cancelsell() {
+      console.log("cancel the onsell equipment")
+    },
+    sellequipment() {
+      if (this.sellprice === "") {
+        alert("Please set the price!")
+      } else {
+        console.log("Sell the equipment")
+        console.log(this.sellprice)
+        this.sellprice = ""
+      }
+    }
   },
 };
 </script>
@@ -59,5 +117,49 @@ li {
 }
 a {
   color: #42b983;
+}
+body {
+  background: #20262E;
+  padding: 20px;
+  font-family: Helvetica;
+}
+
+#app {
+  background: #fff;
+  border-radius: 4px;
+  padding: 20px;
+  transition: all 0.2s;
+}
+
+li {
+  margin: 8px 0;
+}
+
+h2 {
+  font-weight: bold;
+  margin-bottom: 15px;
+}
+
+del {
+  color: rgba(0, 0, 0, 0.3);
+}
+
+.list-group {
+  border: 1px solid black;
+  padding: 5px;
+  margin-top: 5px;
+}
+
+.list-group-item {
+  display: block;
+  transition: all .3s ease-in;
+}
+
+.list-group-item:hover {
+  background: lightgray;
+}
+.list-group-item.active {
+  background: black;
+  color: white;
 }
 </style>
