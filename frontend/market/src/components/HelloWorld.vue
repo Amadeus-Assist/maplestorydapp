7e11e6b1af58993a3f6cbea54c66222d94a780fc
CharlesK <xk2144@columbia.edu>
2022-05-10 01:19:28 -0400

    modify market ui

--- frontend/market/src/components/HelloWorld.vue
+++ frontend/market/src/components/HelloWorld.vue
@@ -1,39 +1,44 @@
 <template>
-  <div class="hello">
+  <div class="controller" v-show="!seemarket">
     <h1 v-show="isConnected">Account: {{ account }}</h1>
     <button v-show="!isConnected" @click="connect">Connect Ale</button>
     <button v-show="isConnected" @click="approveMoney" ref="btnToggle">See All the Items In the Market</button>
-
-  <div class='list-group' v-show="seemarket">
-    This is the list of all onsell equipments.
-  <a v-for='(product, idx) in allonsell' :key="idx" class='list-group-item'
-     v-on:click='toggleActiveIndex(allonsell, idx)' 
-     :class="{'active': idx == activeIndex && allonsell == activeList}" >
-  {{product}}
-  </a>
   </div>
-  <button  @click="buyequipment" v-show="seemarket">Buy</button>
 
-  <div class='list-group' v-show="seemarket">
-    This is the list of all of YOUR onsell equipments.
-     <a v-for='(product, idx) in youronsell' :key="idx" class='list-group-item'
-        v-on:click='toggleActiveIndex(youronsell, idx)' 
-        :class="{'active': idx == activeIndex && youronsell == activeList}" >
-     {{product}} 
-     </a>
-  </div>
-  <button  @click="cancelsell" v-show="seemarket">Cancel the Sell</button>
+  <div class="market" v-show="seemarket">
+    <div class='list-group list-group-local all-on-market'>
+      This is the list of all onsell equipments.
+        <a v-for='(product, idx) in allonsell' :key="idx" class='list-group-item-local list-group-item'
+           v-on:click='toggleActiveIndex(allonsell, idx)'
+           :class="{'list-group-item aria-current': idx == activeIndex && allonsell == activeList}">
+          {{ product }}
+        </a>
+    </div>
 
-  <div class='list-group' v-show="seemarket">
-    This is the list of all of YOUR other equipments.
-     <a v-for='(product, idx) in yourother' :key="idx" class='list-group-item'
-        v-on:click='toggleActiveIndex(yourother, idx)' 
-        :class="{'active': idx == activeIndex && yourother == activeList}" >
-     {{product}} 
-     </a>
-  </div>
-  <input v-model="sellprice" v-show="seemarket" placeholder="Your Price">
-  <button  @click="sellequipment" v-show="seemarket">Sell</button>
+    <div class='list-group list-group-local self-on'>
+      This is the list of all of YOUR onsell equipments.
+      <a v-for='(product, idx) in youronsell' :key="idx" class='list-group-item-local list-group-item'
+         v-on:click='toggleActiveIndex(youronsell, idx)'
+         :class="{'active': idx == activeIndex && youronsell == activeList}">
+        {{ product }}
+      </a>
+    </div>
+
+    <div class='list-group list-group-local self-other'>
+      This is the list of all of YOUR other equipments.
+      <a v-for='(product, idx) in yourother' :key="idx" class='list-group-item-local list-group-item'
+         v-on:click='toggleActiveIndex(yourother, idx)'
+         :class="{'active': idx == activeIndex && yourother == activeList}">
+        {{ product }}
+      </a>
+    </div>
+
+    <div class="buttons">
+      <button @click="buyequipment">Buy</button>
+      <button @click="cancelsell">Cancel the Sell</button>
+      <input v-model="sellprice" placeholder="Your Price">
+      <button @click="sellequipment">Sell</button>
+    </div>
   </div>
 </template>
 
@@ -75,60 +80,60 @@ export default {
       // services.getAllMyNotOnsellEquipment().then((res) => {
       //   console.log(res)
       // });
-      services.getAllMyOnsellEquipment().then((res)=>{
+      services.getAllMyOnsellEquipment().then((res) => {
         this.youronsell = res
       });
-      services.getAllOnsellEquipment().then((res)=>{
+      services.getAllOnsellEquipment().then((res) => {
         this.allonsell = res
       });
-      services.getAllMyNotOnsellEquipment().then((res)=>{
+      services.getAllMyNotOnsellEquipment().then((res) => {
         this.yourother = res
       });
       this.$refs.btnToggle.innerText = "Refresh the Market List"
       this.seemarket = true
     },
-    toggleActiveIndex: function(list,index){
-     this.activeIndex = index;
-     this.activeList = list;
-   },
+    toggleActiveIndex: function (list, index) {
+      this.activeIndex = index;
+      this.activeList = list;
+    },
 
     buyequipment() {
       console.log("buy the onsell equipment")
       var id = this.allonsell[this.activeIndex].id
       var price = this.allonsell[this.activeIndex].price
-        console.log("id is ", id)
-        console.log("price is ", price)
-        services.BuyEquipment(id, price).then(()=>{
-          // refresh the list
-          services.getAllMyOnsellEquipment().then((res)=>{
+      console.log("id is ", id)
+      console.log("price is ", price)
+      services.BuyEquipment(id, price).then(() => {
+        // refresh the list
+        services.getAllMyOnsellEquipment().then((res) => {
           this.youronsell = res
-          });
-          services.getAllOnsellEquipment().then((res)=>{
-            this.allonsell = res
-          });
-          services.getAllMyNotOnsellEquipment().then((res)=>{
-            this.yourother = res
-          });
-          this.sellprice = ""
-        })
+        });
+        services.getAllOnsellEquipment().then((res) => {
+          this.allonsell = res
+        });
+        services.getAllMyNotOnsellEquipment().then((res) => {
+          this.yourother = res
+        });
+        this.sellprice = ""
+      })
     },
     cancelsell() {
       console.log("cancel the onsell equipment")
       var id = this.youronsell[this.activeIndex].id
-        console.log("id is ", id)
-        services.CancelSellEquipment(id).then(()=>{
-          // refresh the list
-          services.getAllMyOnsellEquipment().then((res)=>{
+      console.log("id is ", id)
+      services.CancelSellEquipment(id).then(() => {
+        // refresh the list
+        services.getAllMyOnsellEquipment().then((res) => {
           this.youronsell = res
-          });
-          services.getAllOnsellEquipment().then((res)=>{
-            this.allonsell = res
-          });
-          services.getAllMyNotOnsellEquipment().then((res)=>{
-            this.yourother = res
-          });
-          this.sellprice = ""
-        })
+        });
+        services.getAllOnsellEquipment().then((res) => {
+          this.allonsell = res
+        });
+        services.getAllMyNotOnsellEquipment().then((res) => {
+          this.yourother = res
+        });
+        this.sellprice = ""
+      })
     },
     sellequipment() {
       console.log("sell my equipment")
@@ -136,19 +141,19 @@ export default {
         alert("Please set the price!")
       } else {
         console.log(this.sellprice)
-        
+
         console.log(this.yourother[this.activeIndex])
         var id = this.yourother[this.activeIndex].id
         console.log("id is ", id)
-        services.SellEquipment(id, this.sellprice).then(()=>{
+        services.SellEquipment(id, this.sellprice).then(() => {
           // refresh the list
-          services.getAllMyOnsellEquipment().then((res)=>{
-          this.youronsell = res
+          services.getAllMyOnsellEquipment().then((res) => {
+            this.youronsell = res
           });
-          services.getAllOnsellEquipment().then((res)=>{
+          services.getAllOnsellEquipment().then((res) => {
             this.allonsell = res
           });
-          services.getAllMyNotOnsellEquipment().then((res)=>{
+          services.getAllMyNotOnsellEquipment().then((res) => {
             this.yourother = res
           });
           this.sellprice = ""
@@ -164,17 +169,21 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+
+/*ul {*/
+/*  list-style-type: none;*/
+/*  padding: 0;*/
+/*}*/
+
+/*li {*/
+/*  display: inline-block;*/
+/*  margin: 0 10px;*/
+/*}*/
+
 a {
   color: #42b983;
 }
+
 body {
   background: #20262E;
   padding: 20px;
@@ -201,13 +210,38 @@ del {
   color: rgba(0, 0, 0, 0.3);
 }
 
-.list-group {
-  border: 1px solid black;
-  padding: 5px;
-  margin-top: 5px;
+.market {
+  width: 95vw;
+  height: 85vh;
+  padding: 2.5vw;
 }
 
-.list-group-item {
+.all-on-market {
+  width: 50%;
+  height: 100%;
+  float: left;
+}
+
+.self-on {
+  width: 45%;
+  height: 40%;
+  float: right;
+}
+
+.self-other {
+  width: 45%;
+  height: 40%;
+  float: right;
+  margin-top: 10px;
+}
+
+.list-group-local {
+  border: 1px solid black;
+  padding: 5px;
+}
+
+.list-group-item-local {
+  float: right;
   display: block;
   transition: all .3s ease-in;
 }
@@ -215,8 +249,16 @@ del {
 .list-group-item:hover {
   background: lightgray;
 }
+
 .list-group-item.active {
   background: black;
   color: white;
 }
+
+.buttons {
+  width: 45%;
+  float: right;
+  margin-top: 10px;
+}
+
 </style>
